f9a8efaf224fa6424a5e9de1329374814d00dbac
Kyle P Clarkson <kyleclarkson17@hotmail.com>
2021-06-06 19:35:50 -0700

    Added map component.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import api from "./weather_api.js";
 
 import SearchBar from "./components/SearchBar";
 import Weather from "./components/Weather";
+import Map from "./components/Map"
 
 function App() {
   // The city to query weather data.
@@ -37,41 +38,15 @@ function App() {
 
   const getNeighboringWeatherData = async ({ lat, lon }) => {
     const response = await fetch(
-      `${api.API_BASE_URL}find?lat=${lat}&lon=${lon}&cnt=${NUM_NEIGHBORING_CITIES}&appid=${api.API_KEY}`
+      `${api.API_BASE_URL}find?lat=${lat}&lon=${lon}&cnt=${NUM_NEIGHBORING_CITIES}&units=metric&appid=${api.API_KEY}`
     );
     if (response.ok) {
       const data = await response.json();
+      setNeighborWeather(data);
       console.log("Neighbors", data);
     } else {
       console.log("Error fetching data for cities.");
     }
-  };
-
-  // Converts wind direction (0,360 deg) to compass direction (90 E, 180 S, etc.)
-  const getWindDirection = (deg) => {
-    if (22.5 <= deg && deg < 67.5) {
-      return "NE";
-    } else if (67.5 <= deg && deg < 112.5) {
-      return "E";
-    } else if (112.5 <= deg && deg < 157.5) {
-      return "SE";
-    } else if (157.5 <= deg && deg < 202.5) {
-      return "S";
-    } else if (202.5 <= deg && deg < 247.5) {
-      return "SW";
-    } else if (247.5 <= deg && deg < 292.5) {
-      return "W";
-    } else if (292.5 <= deg && deg < 337.5) {
-      return "NW";
-    } else {
-      return "N";
-    }
-  };
-
-  // Returns unix_time to local time string.
-  const convertTime = (unix_time) => {
-    const seconds_offset = new Date(0).setUTCSeconds(unix_time);
-    return new Date(seconds_offset).toLocaleTimeString();
   };
 
   return (
@@ -92,7 +67,10 @@ function App() {
         </div>
         <div className="col-md-8 col-sm-12 map">
           {neighborWeather ? (
-            <div></div>
+            <Map
+              cityCoords={cityWeather.coord}
+              neighborWeather={neighborWeather}
+            />
           ) : (
             // Empty
             ""
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import { MapContainer} from 'react-leaflet';
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { DivIcon } from "leaflet"
 // CSS
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
+import "./map.css";
 
-const Map = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Map = ({ cityCoords, neighborWeather }) => {
 
-export default Map
+  const [activeCity, setActiveCity] = React.useState(null);
+
+  return (
+    <div className="map-container">
+      <MapContainer center={[cityCoords.lat, cityCoords.lon]} zoom={8}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {neighborWeather.list.map((city) => (
+          <Marker
+            key={city.id}
+            position={[city.coord.lat, city.coord.lon]}
+            icon={new DivIcon({
+                className: "map-icon",
+                iconSize: [50, 50],
+                html: `<strong>${Math.round(city.main.temp)}°C</strong>`
+            })}
+          >
+            <Popup
+              position={[city.coord.lat, city.coord.lon]}
+            >
+              <strong>{city.name}</strong>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
